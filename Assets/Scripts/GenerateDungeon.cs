cff4574eecff57070a2204ca73582d59a2d199f9
KopIAkash1 <andreymart01@gmail.com>
2025-02-18 18:34:19 +0300

    Generate Dungeon base logic (Matrix base)

---
+++ Assets/Scripts/GenerateDungeon.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateDungeon : MonoBehaviour {
+    [SerializeField] public int heightMatrix = 6;
+    [SerializeField] public int weightMatrix = 5;
+    [SerializeField] public int roomsCount = 6;
+
+    private int[,] matrix;
+    private Dictionary<int, (int, int)> roomsAddress = new Dictionary<int, (int, int)>();
+    void Start()
+    {
+        getGeneratedMatrix();
+        printMatrix();
+    }
+
+    public int[,] getGeneratedMatrix(){
+        matrix = new int[heightMatrix,weightMatrix];
+        for(int i = 0; i < heightMatrix; i++){
+            for (int j = 0; j < weightMatrix; j++) {
+                matrix[i,j] = -1;
+            }
+        }
+        matrix = generateRooms();
+        return matrix;
+    }
+    private int[,] generateRooms(){
+        matrix = new int[heightMatrix,weightMatrix];
+        for(int i = 0; i < heightMatrix; i++){
+            for (int j = 0; j < weightMatrix; j++) {
+                matrix[i,j] = -1;
+            }
+        }
+        // generate first room
+        Unity.Mathematics.Random rand = new Unity.Mathematics.Random((uint)DateTime.Now.Ticks);
+        int firstX = rand.NextInt(0,weightMatrix);
+        int firstY = rand.NextInt(0,heightMatrix);
+        matrix[firstY, firstX] = 0;
+        roomsAddress.Add(0,(firstY, firstX));
+        // MAIN GEN (generating other rooms)
+
+       for (int i = 1; i < roomsCount; i++){
+            int currentRoom = i;
+            if (i % 4 == 0) {
+                currentRoom = Mathf.Abs(rand.NextInt(0, i - 1));
+            }
+            (int choosenRoomX, int choosenRoomY) = roomsAddress[Mathf.Abs(currentRoom - 1)];
+            
+            int direction = rand.NextInt(0, 4);
+            int newX = choosenRoomX;
+            int newY = choosenRoomY;
+
+            switch (direction) {
+                case 0: newX += 1; break;
+                case 1: newX -= 1; break;
+                case 2: newY += 1; break;
+                case 3: newY -= 1; break;
+            }
+
+            if (newX >= 0 && newX < heightMatrix && newY >= 0 && newY < weightMatrix && matrix[newX,newY] == -1) {
+                matrix[newX, newY] = i;
+                roomsAddress.Add(i,(newX,newY));
+            } else {
+                i--;
+            }
+        }
+        return matrix;
+    }
+
+    private void printMatrix(){
+        string buffer = "";
+        for (int i = 0; i < heightMatrix; i++ ) {
+            for (int j = 0; j < weightMatrix; j++ ) {
+                buffer += "|" + matrix[i,j] + "|";
+            }
+            buffer += "\n";
+        }
+        Debug.Log(buffer);
+    }
+}
