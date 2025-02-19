12e045a9a7946792e6d3b5b81a4dde6d4632354c
shokii <shilo152004@mail.ru>
2025-02-19 21:22:50 +0300

    Start core logic for player
    Make flip via mouse pos

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public int health = 100;
+    [SerializeField] public float moveSpeed = 5f;
+    
+    private Vector2 _mousePosition;
+
+    // TODO:
+    //  - Базово: Ношение предмета \\ При лучшем исходе: поднятие предмета
+    //  - сделать дэш в сторону отсносительно мыши, а не в направление движения
+
+    private void FixedUpdate()
+    {
+        MouseRotation();
+    }
+    
+    private void MouseRotation()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GetComponent<SpriteRenderer>().flipX = _mousePosition.x < transform.position.x;
+    }
+
+    private void Dash()
+    {
+        
+    }
+
+    private void ItemPickUp()
+    {
+        
+    }
+
+    private void ItemDrop()
+    {
+        
+    }
+}
