56b99aaaa417cd8f48a4552a61b11244b8ecf72f
KopIAkash1 <andreymart01@gmail.com>
2025-02-25 21:52:50 +0300

    player collider reworked to tags logic

--- Assets/Scripts/PlayerColliderSettings.cs
+++ Assets/Scripts/PlayerColliderSettings.cs
@@ -1,18 +1,38 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class PlayerCollisionSettings : MonoBehaviour
 {
-    public Collider2D wallCollider;
-    public Collider2D playerCollider;
+    //public Collider2D wallCollider;
+    private GameObject[] _walls;
+    private GameObject _player;
+    //public Collider2D playerCollider;
 
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _walls = GameObject.FindGameObjectsWithTag("Wall");
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("ENTER");
-        Physics2D.IgnoreCollision(playerCollider, wallCollider, true);
+        Collider2D playerCollider = _player.GetComponent<Collider2D>();
+        foreach (GameObject wall in _walls)
+        {
+            Collider2D wallCollider = wall.GetComponent<Collider2D>();
+            Physics2D.IgnoreCollision(playerCollider, wallCollider, true);
+        }
+        //Physics2D.IgnoreCollision(playerCollider, wallCollider, true);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("EXIT");
-        Physics2D.IgnoreCollision(playerCollider, wallCollider, false);
+        Collider2D playerCollider = _player.GetComponent<Collider2D>();
+        foreach (GameObject wall in _walls)
+        {
+            Collider2D wallCollider = wall.GetComponent<Collider2D>();
+            Physics2D.IgnoreCollision(playerCollider, wallCollider, false);
+        }
     }
 }
