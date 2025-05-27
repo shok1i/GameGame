2b2a52182e0a0eb8f232bb1b3626093422cab7c5
KopIAkash1 <andreymart01@gmail.com>
2025-05-27 05:14:11 +0300

    necromancer added

--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
@@ -10,6 +12,10 @@ public class EnemyManager : MonoBehaviour
     public float speed;
     public float distanceToFollow;
     public float distanceToAttack;
+
+    public bool isDistanceFighter;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
     private float distance;
 
     private bool isAttacking = false;
@@ -26,40 +32,81 @@ public class EnemyManager : MonoBehaviour
         distance = Vector2.Distance(transform.position, target.transform.position);
         Vector2 direction = target.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        if (distance < distanceToFollow)
+        if (!isDistanceFighter)
         {
-            if (target.transform.position.x < transform.position.x)
+            if (distance < distanceToFollow)
             {
-                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+                if (target.transform.position.x < transform.position.x)
+                {
+                    transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+                }
+                if (distance < distanceToAttack)
+                {
+                    if (!isAttacking)
+                    {
+                        isAttacking = true;
+                        StartCoroutine(AttackCoroutine());
+                    }
+                }
+                if (!isAttacking && !isDeath)
+                {
+                    transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
+                    _animator.SetBool("IsRunning", true);
+                }
             }
             else
             {
-                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+                _animator.SetBool("IsRunning", false);
             }
+        }
+        else if (isDistanceFighter)
+        {
+            if (isAttacking) return;
+            if (target.transform.position.x < transform.position.x)
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+            else
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+
             if (distance < distanceToAttack)
             {
-                if (!isAttacking)
-                {
-                    isAttacking = true;
-                    StartCoroutine(AttackCoroutine());
-                }
-            }
-            else if (!isAttacking && !isDeath)
-            {
-                transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
+                Vector2 directionAway = (transform.position - target.transform.position).normalized;
+                Vector2 targetPos = (Vector2)transform.position + directionAway * speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
                 _animator.SetBool("IsRunning", true);
             }
+            else if (distance <= distanceToFollow)
+            {
+                if (distance <= distanceToAttack + 0.1f)
+                {
+                    _animator.SetBool("IsRunning", false);
+                    if (!isAttacking)
+                    {
+                        isAttacking = true;
+                        StartCoroutine(AttackDistanceCoroutine());
+                    }
+                }
+                else
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                    _animator.SetBool("IsRunning", true);
+                    isAttacking = false;
+                }
+            }
+            else
+            {
+                _animator.SetBool("IsRunning", false);
+                isAttacking = false;
+            }
         }
-        else
-        {
-            _animator.SetBool("IsRunning", false);
-        }
+
     }
     IEnumerator AttackCoroutine()
     {
-        isAttacking = true;
+        var rb = gameObject.GetComponent<Rigidbody2D>();
         _animator.SetBool("IsAttacking", true);
         _animator.SetBool("IsRunning", false);
         yield return new WaitUntil(() => _animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"));
@@ -70,12 +117,30 @@ public class EnemyManager : MonoBehaviour
         }
         _animator.SetBool("IsAttacking", false);
         yield return new WaitForSeconds(0.5f);
+        Debug.Log("Cooldown object" + gameObject);
+        isAttacking = false;
+    }
+
+    IEnumerator AttackDistanceCoroutine()
+    {
+        Debug.Log("Starting distance attack");
+        _animator.SetBool("IsAttacking", true);
+        _animator.SetBool("IsRunning", false);
+        yield return new WaitUntil(() => _animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"));
+        yield return new WaitUntil(() => _animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
+        if (distance > distanceToAttack)
+        {
+            Debug.Log("Called method");
+            _distanceAttack();
+        }
+        _animator.SetBool("IsAttacking", false);
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Shooted");
         isAttacking = false;
     }
 
     IEnumerator DeathCoroutine()
     {
-        Debug.Log("deleting object " + gameObject);
         _animator.SetBool("IsAttacking", false);
         _animator.SetBool("IsRunning", false);
         AnimatorStateInfo state = _animator.GetCurrentAnimatorStateInfo(0);
@@ -95,6 +160,17 @@ public class EnemyManager : MonoBehaviour
         isAttacking = false;
     }
 
+
+    private void _distanceAttack()
+    {
+        Vector2 direction = (target.transform.position - gameObject.transform.position).normalized;
+        GameObject bullet = Instantiate(bulletPrefab, gameObject.transform.position, Quaternion.identity);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = direction * bulletSpeed;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
+        Debug.Log("Bullet created");
+    }
     private void _attack()
     {
         target.GetComponent<PlayerManager>().GetComponent<PlayerHealth>().TakeDamage(damage);
--- Assets/Scripts/Items/Shooting.cs
+++ Assets/Scripts/Items/Shooting.cs
@@ -6,6 +6,8 @@ public class Bullet : MonoBehaviour
     public GameObject hitEffectPrefab;
     public AudioClip hitSound;
     public float effectLifetime = 1f; 
+    public bool enemyShooting = false;
+    public float enemyDamage;
     private AudioSource audioSource;
 
     void Start()
@@ -21,24 +23,43 @@ public class Bullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Wall") || other.CompareTag("Enemy"))
+        if (other.CompareTag("Wall"))
         {
-            if (other.CompareTag("Enemy"))
-            {
-                EnemyManager enemy = other.GetComponent<EnemyManager>();
-                enemy.getDamage(20f); // обновить для Weapon
-            }
             if (hitEffectPrefab != null)
             {
                 GameObject effect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
                 Destroy(effect, effectLifetime);
             }
-
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Player") && enemyShooting)
+        {
+            if (hitEffectPrefab != null)
+            {
+                GameObject effect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+                Destroy(effect, effectLifetime);
+            }
             if (hitSound != null)
             {
                 audioSource.PlayOneShot(hitSound);
             }
-
+            PlayerManager player = other.GetComponent<PlayerManager>();
+            player.playerHealth.TakeDamage(enemyDamage);
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Enemy") && !enemyShooting)
+        {
+            if (hitEffectPrefab != null)
+            {
+                GameObject effect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+                Destroy(effect, effectLifetime);
+            }
+            if (hitSound != null)
+            {
+                audioSource.PlayOneShot(hitSound);
+            }
+            EnemyManager enemy = other.GetComponent<EnemyManager>();
+            enemy.getDamage(20f);
             Destroy(gameObject);
         }
     }
